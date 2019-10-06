a1202a23c139ba98b0d3d44db27020ee73972717
Ilham Israfilov <ilham.israfilov@gmail.com>
2019-10-06 16:39:32 +0400

    Add project files.

---
+++ LogEventArchiver/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogEventArchiver
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
